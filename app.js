8fbd788ced79ee840308c348ec1752040fe3fc09
Calvin G Hall <123chall@gmail.com>
2019-07-02 11:34:18 -0500

    More array stuff

--- app.js
+++ app.js
@@ -13,3 +13,24 @@ console.log("Popping tail...")
 movieVillains.pop(movieVillains.length - 1);
 console.log(`Array of Villains (step ${step++}): ${movieVillains}.`);
 console.log(`Contains Red: ${movieVillains.includes("Red")}.`);
+console.log("Now lets remove red...");
+movieVillains.splice(movieVillains.indexOf("Red"), 1);
+console.log(`Array of Villains (step ${step++}): ${movieVillains}.`);
+console.log("Now lets replace Mew-Two with Jiren...");
+movieVillains.splice(movieVillains.indexOf("Mew-Two"), 1, "Jiren");
+console.log(`Array of Villains (step ${step++}): ${movieVillains}.`);
+console.log("Now lets reverse the array...");
+movieVillains.reverse();
+console.log(`Array of Villains (step ${step++}): ${movieVillains}.`);
+console.log("Now lets \"pop\" the front of the array...");
+movieVillains.shift();
+console.log(`Array of Villains (step ${step++}): ${movieVillains}.`);
+
+function testConstructor(testName, testData) {
+  return {name: testName,
+          data: testData};
+}
+
+var testObj = testConstructor("Test Object", testConstructor("Test Data", "Test Jibberish"));
+
+console.log(testObj);
